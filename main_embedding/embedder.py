12f6a614abdea2aef778a1d72c5ce7cdefce3c7b
Lasklinikov <wuchenyuan@mail.ustc.edu.cn>
2024-11-19 00:30:41 +0800

    add code

---
+++ main_embedding/embedder.py
@@ -0,0 +1,215 @@
+import torch
+from blingfire import text_to_sentences_and_offsets
+from sentence_transformers import SentenceTransformer
+from typing import List
+
+class SentenceExtractor:
+    def _extract_sentences(self, context):
+        """
+        Extracts and returns sentences from given context.
+        Parameters:
+            context (str): Input document context.
+        Returns:
+        """
+        # Extract offsets of sentences from the text
+        _, offsets = text_to_sentences_and_offsets(context)
+
+        # Initialize a list to store sentences
+        sentences = []
+
+        # Iterate through the list of offsets and extract sentences
+        for start, end in offsets:
+            # Extract the sentence and limit its length
+            sentence = context[start:end]
+            sentences.append(sentence)
+
+        return sentences
+
+class SentenceEmbedder:
+    def __init__(self, accelerator=None):
+        self.sentence_model = SentenceTransformer(
+            "sentence-transformers/all-MiniLM-L6-v2",
+            device=torch.device(
+                "cuda" if torch.cuda.is_available() else "cpu"
+            ),
+        )
+        if accelerator:
+            self.sentence_model.to(accelerator.device)
+        self.sentence_extractor = SentenceExtractor()
+
+    def calculate_embeddings(self, sentences):
+        """
+        Compute normalized embeddings for a list of sentences using a sentence encoding model.
+
+        This function leverages multiprocessing to encode the sentences, which can enhance the
+        processing speed on multi-core machines.
+
+        Args:
+            sentences (List[str]): A list of sentences for which embeddings are to be computed.
+
+        Returns:
+            np.ndarray: An array of normalized embeddings for the given sentences.
+
+        """
+        embeddings = self.sentence_model.encode(
+            sentences=sentences,
+            normalize_embeddings=True,
+            batch_size=128,
+        )
+        # Note: There is an opportunity to parallelize the embedding generation across 4 GPUs
+        #       but sentence_model.encode_multi_process seems to interefere with Ray
+        #       on the evaluation servers. 
+        #       todo: this can also be done in a Ray native approach.
+        #       
+        return embeddings
+
+    def estimate_importance(self, context, query):
+        sentences = self.sentence_extractor._extract_sentences(context)
+        sentences_embeddings = self.calculate_embeddings(sentences)
+        query_embeddings = self.calculate_embeddings(query)
+        # Calculate cosine similarity between query and chunk embeddings,
+        cosine_scores = (sentences_embeddings * query_embeddings).sum(1)
+        
+        return sentences, (-cosine_scores).argsort()
+
+
+def extract_sentences_from_encoder_input_ids(
+    encoder_input_ids: List[List[int]],
+    tokenizer,
+    sentence_extractor,
+):
+    context_list = []
+    sentences_list = []
+    deviation_list = []
+    for _encoder_input_ids in encoder_input_ids:
+        encoder_length = len(_encoder_input_ids)
+        context = tokenizer.decode(_encoder_input_ids)
+
+        length_deviation = len(tokenizer.encode(context)) - encoder_length
+        sentences = sentence_extractor._extract_sentences(context)
+
+        context_list.append(context)
+        sentences_list.append(sentences)
+        deviation_list.append(length_deviation)
+    
+    return context_list, sentences_list, deviation_list
+
+
+def get_sentence_begin_indices(
+    encoder_input_ids: List[List[int]],
+    tokenizer,
+    sentence_extractor,
+):
+    sentence_begin_indices_list = []
+    context_list, sentences_list, deviation_list = extract_sentences_from_encoder_input_ids(encoder_input_ids, tokenizer, sentence_extractor)
+    number_of_sentences = []
+    idx1 = 0
+    sentence_number = 0
+    
+    # * extract each sentences' begin index in encoder_input_ids, considering length deviation
+    for context, _sentences_list, deviation in zip(context_list, sentences_list, deviation_list):
+        sentence_begin_indices = [{'idx1': idx1, 'idx2': 0}]
+        former_index1 = 0
+        for i in range(len(_sentences_list) - 1):
+
+            sentence1 = _sentences_list[i]
+            sentence2 = _sentences_list[i + 1]
+
+            split1 = context[former_index1:].split(sentence1)[0] + sentence1
+            split1_total = context[:former_index1] + split1  
+            former_index1 += len(split1)
+
+            split2 = context[former_index1:].split(sentence2)[0] + sentence2
+            split2_total = context[:former_index1] + split2
+
+            indices1 = tokenizer.encode(split1_total)
+            indices2 = tokenizer.encode(split2_total)
+            if split1_total != context[:former_index1]:
+                print('former_index1', former_index1)
+                exit()
+            for j in range(len(indices1)):
+                if j - deviation > 4095: #FIXME assume lm_max_length=4096
+                    print('deviation', deviation)
+                    exit()
+                if indices1[j] != indices2[j]:
+                    sentence_begin_indices.append({'idx1': idx1, 'idx2': j - deviation})
+                    break
+            if (j == len(indices1) - 1) and (indices1[j] == indices2[j]):
+                sentence_begin_indices.append({'idx1': idx1, 'idx2': j + 1 - deviation})
+        
+        # append the total length at the end for subsequent calculations
+        sentence_begin_indices.append({'idx1': idx1, 'idx2': len(encoder_input_ids[idx1])})
+        sentence_number += len(_sentences_list)
+
+        number_of_sentences.append(sentence_number)
+
+        idx1 += 1
+
+        sentence_begin_indices_list.extend(sentence_begin_indices)
+    
+    return  number_of_sentences, sentences_list, sentence_begin_indices_list
+
+
+def get_sentence_priority_list(
+    sentences_list: List[List[str]],
+    sentence_embedder,
+    prompt,
+    number_of_sentences,
+    ):
+    sentence_priority_list = [],
+    # * Convert List[List[str]] to List[str]
+    sentences = []
+    for _sentences_list in sentences_list: 
+        sentences.extend(_sentences_list)
+
+    # * Extract query, maybe not fitted at all time
+    query = prompt.split('Question: ')[1].split("\nAnswer:")[0]
+    # print(query)
+
+    sentences_embeddings = sentence_embedder.calculate_embeddings(sentences)
+    query_embeddings = sentence_embedder.calculate_embeddings(query)
+    # * Calculate cosine similarity between query and chunk embeddings,
+    cosine_scores = (sentences_embeddings * query_embeddings).sum(1)
+
+    priority_list = (-cosine_scores).argsort()
+
+    # * Handle the impact of changes in idx caused by inserting total length before processing
+    for number in number_of_sentences[::-1]:
+        temp = []
+        for sentence_id in priority_list:
+            if sentence_id >= number:
+                temp.append(sentence_id + 1)
+            else:
+                temp.append(sentence_id)
+        priority_list = temp
+
+    # * Get sentences_ids_list in encoder_input_ids split way for encoder_indices generation
+    sentences_ids = sorted(priority_list)
+    temp = [] 
+    sentences_ids_list = []
+    for idx in range(len(sentences_ids)):
+        temp.append(sentences_ids[idx])
+        if (idx == len(sentences_ids) - 1) or (sentences_ids[idx] + 1 != sentences_ids[idx + 1]):
+            sentences_ids_list.append(temp)
+            temp = []
+
+    
+    return priority_list, sentences_ids_list
+
+
+# merge two list
+def merge(list1, list2):
+    combined_set = set(list1) | set(list2)
+    return list(combined_set)
+
+    
+
+    
+
+
+        
+    
+
+        
+
+
---
+++ main_embedding/eval_open_domain_qa.py
@@ -0,0 +1,260 @@
+import os
+import datasets
+import uuid
+import logging
+import random
+from dataclasses import dataclass, field, asdict
+from typing import List
+from transformers import HfArgumentParser, set_seed
+from transformers.tokenization_utils import PreTrainedTokenizer
+from accelerate import Accelerator
+from src.data import FlexRAGCollator, DefaultDataCollator
+from src.open_domain_qa.config import DATASET2PROMPT, DATASET2MAXLEN, DATASET2METRIC
+from src.open_domain_qa.metric import Metric
+from src.model import load_model_and_tokenizer
+from src.args import ModelArgs, LoraArgs
+from torch.utils.data import DataLoader
+from src.utils import save_to_json, FileLogger, move_to_device
+from src.data import Data, INPUT_TAG, CONTEXT_TAG
+from tqdm import tqdm
+
+
+@dataclass
+class TaskArgs:
+    cpu: bool = field(
+        default=False,
+    )
+    data_dir: str = field(
+        default="data/open_domain_qa",
+    )
+    dataset_names: List[str] = field(
+        default=lambda: [],
+    )
+    chat_template: str = field(
+        default="llama-2",
+    )
+    retrieval_num: int = field(
+        default=5,
+    )
+    comp_ratio: int = field(
+        default=1,
+    )
+    down_scaling_method: str = field(
+        default="stride",
+    )
+    batch_size: int = field(
+        default=1,
+    )
+    output_dir: str = field(
+        default="data/results/rag"
+    )
+    seed: int = field(
+        default=42,
+    )
+    ratio_power_of_two: bool = field( 
+        default=True,
+    ) # whether use ratio which is power of two for dynamic compression
+    use_encoder_at_ratio_one: bool = field(
+        default=False,
+    )
+    sentence_dir: str = field(
+        default="",
+    )
+    text_proportion: float = field(
+        default=0.1,
+    ) # the proportion of the importance context in the original context  
+    low_comp_ratio: int = field(
+        default=1,
+    ) # the compression ratio for important context
+
+    def __post_init__(self):
+        if len(self.dataset_names) == 0:
+            raise ValueError("`dataset_names` can not be empty.")
+
+
+def prepare_open_domain_qa(data_dir: str , dataset_names: List[str], tokenizer: PreTrainedTokenizer, retrieval_num: int, seed: int):
+    def _process(data: dict, dataset_name: str, retrieval_num: int):
+        # * get prompt and replace query placehoder
+        prompt = DATASET2PROMPT[dataset_name]
+        prompt = prompt.replace(INPUT_TAG, data["query"])
+
+        # * format context
+        retrieval_results = data["key"]
+        # print(len(data["key"]))
+        context = "\n\n".join([
+            f"Doc {i + 1}: {retrieval_results[i]}"
+            for i in range(retrieval_num)
+        ])
+
+        # * get content
+        content = prompt.replace(CONTEXT_TAG, context)
+        
+        # * format return
+        return {
+            "conversations": [
+                {
+                    "content": content,
+                    "role": "user",
+                    "prompt": prompt,
+                    "context": context,
+                },
+                {"content": None, "role": "assistant"},
+            ],
+        }
+
+    dataset_dict = datasets.DatasetDict()
+    for dataset_name in dataset_names:
+        path = os.path.join(data_dir, f"{dataset_name}.json")
+        dataset = datasets.load_dataset("json", data_files=path, split="train")
+        
+        dataset_dict[dataset_name] = dataset.map(
+            _process,
+            fn_kwargs={
+                "dataset_name": dataset_name,
+                "retrieval_num": retrieval_num,
+            },
+            num_proc=32,
+            desc=f"prepare {dataset_name}",
+        )
+    
+    return dataset_dict
+
+def load_importance_sentence_indices(data_dir: str, dataset_names: List[str], chat_template: str, text_proportion: float):
+    dataset_dict = {}
+    for dataset_name in dataset_names:
+        data_name = dataset_name + "." + str(text_proportion) + ".json"
+        path = os.path.join(data_dir, chat_template, data_name)
+        dataset = datasets.load_dataset("json", data_files=path, split="train")
+        dataset_dict[dataset_name] = dataset
+    
+    return dataset_dict
+
+def main():
+    # * set parser
+    parser = HfArgumentParser([ModelArgs, LoraArgs, TaskArgs])
+    model_args, lora_args, task_args = parser.parse_args_into_dataclasses()
+
+    # * set logging
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+    )
+
+    # * set seed
+    set_seed(task_args.seed)
+
+    # * set device
+    accelerator = Accelerator(cpu=task_args.cpu)
+
+    # * model and tokenizer
+    model, tokenizer = load_model_and_tokenizer(model_args, lora_args, accelerator=accelerator)
+    tokenizer.padding_side = "left"
+
+    # * load dataset and process
+    with accelerator.main_process_first():
+        dataset_dict = prepare_open_domain_qa(
+            data_dir=task_args.data_dir,
+            dataset_names=task_args.dataset_names,
+            tokenizer=tokenizer,
+            retrieval_num=task_args.retrieval_num,
+            seed=task_args.seed,
+        )
+        tokenizer.add_tokens([CONTEXT_TAG], special_tokens=True)
+        temp_dict = {}
+
+        # * load llmlingua prepcoessed importance_token_indices_list
+        importance_sentence_dicts = load_importance_sentence_indices(data_dir=task_args.sentence_dir, dataset_names=task_args.dataset_names, chat_template=task_args.chat_template,
+        text_proportion=task_args.text_proportion)
+
+        for dataset_name in dataset_dict.keys():
+            importance_sentence_dict = importance_sentence_dicts[dataset_name]
+
+            temp_dict[dataset_name] = dataset_dict[dataset_name].map(
+                Data.process_flexrag_embedding_sc,
+                fn_kwargs={
+                    "tokenizer": tokenizer,
+                    "chat_template": task_args.chat_template,
+                    "lm_max_length": model_args.lm_max_length,
+                    "encoder_max_length": model_args.encoder_max_length,
+                    "comp_candidates": [task_args.comp_ratio],
+                    "down_scaling_method": task_args.down_scaling_method,
+                    "eval_mode": True,
+                    "dataset_name": dataset_name, 
+                    "ratio_power_of_two": task_args.ratio_power_of_two,
+                    "use_encoder_at_ratio_one": task_args.use_encoder_at_ratio_one,
+                    "text_proportion": task_args.text_proportion,
+                    "low_comp_ratio": task_args.low_comp_ratio,
+                    "importance_sentence_dict": importance_sentence_dict,
+                },
+                with_indices=True,
+                batched=True,
+                num_proc=32,
+            )
+        
+        dataset_dict.update(temp_dict)
+    
+    # * eval open domain qa
+    task_id = str(uuid.uuid4()).replace("-", "")
+
+    metrics_dict = {}
+    for dataset_name in dataset_dict:
+        # * prepare dataloader
+        if task_args.comp_ratio ==0:
+            collator = DefaultDataCollator(tokenizer)
+        else:
+            collator = FlexRAGCollator(tokenizer)
+        dataloader = DataLoader(
+            dataset_dict[dataset_name],
+            batch_size=task_args.batch_size,
+            collate_fn=collator,
+            pin_memory=True,
+        )
+        dataloader = accelerator.prepare(dataloader)
+
+        # * generate
+        generations = []
+        for inputs in tqdm(dataloader, desc=f"eval rag: {dataset_name}"):
+            inputs = move_to_device(inputs, model.device)
+            inputs = Data.format_inputs(inputs)
+            outputs = model.generate(
+                **inputs,
+                use_cache=True,
+                max_new_tokens=DATASET2MAXLEN[dataset_name],
+                num_beams=1,
+                do_sample=False,
+                temperature=1.0,
+                pad_token_id=tokenizer.eos_token_id,
+            )
+            outputs = outputs[:, inputs["input_ids"].shape[1]:]
+
+            if accelerator.num_processes > 1:
+                outputs = outputs.contiguous()  # must be contiguous
+                outputs = accelerator.pad_across_processes(outputs, pad_index=tokenizer.pad_token_id, dim=1)
+                outputs = accelerator.gather_for_metrics(outputs)
+            
+            outputs = outputs.tolist()
+            outputs = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+            
+            generations.extend(outputs)
+
+        if accelerator.process_index == 0:
+            # * evaluate
+            answers = dataset_dict[dataset_name]["answers"]
+            metrics = Metric.compute([x.split("\n")[0] for x in generations], answers, DATASET2METRIC[dataset_name])
+            metrics_dict[dataset_name] = metrics
+            
+            # * save
+            questions = dataset_dict[dataset_name]["query"]
+            save_to_json(
+                os.path.join(task_args.output_dir, task_id, f"{dataset_name}.json"),
+                [{"question": question, "output": output, "answers": _answers} for question, output, _answers in zip(questions, generations, answers)],
+            )
+            
+    # * log metric
+    if accelerator.process_index == 0:
+        file_logger = FileLogger(os.path.join(task_args.output_dir, "open_domain_qa.log"))
+        file_logger.log(metrics_dict, ModelArgs=asdict(model_args), TaskArgs=asdict(task_args), uuid=task_id)
+
+if __name__ == "__main__":
+    main()
